b5bcf55936b64085e9c5cfb6279645bc1d063597
Andrei Olar <andrei.olar@samlex.ro>
2022-10-01 13:30:56 +0300

    Refactor utils

--- src/invoice_utils/__main__.py
+++ src/invoice_utils/__main__.py
@@ -6,12 +6,12 @@ from invoice_utils.models import InvoicedItem
 from invoice_utils.engine import InvoicingEngine
 from invoice_utils.render import PdfInvoiceRenderer
 
+root_dir = Path(__file__).parent
+basic_rules = str(root_dir / "basic.json")
+out_path = str(root_dir / "out.pdf")
 
-templates_dir = Path(__file__).parent / "templates"
-basic_invoice_data_template = templates_dir / "basic.json"
-
-engine = InvoicingEngine(str(basic_invoice_data_template))
+engine = InvoicingEngine(str(basic_rules))
 context = engine.process(1, datetime.now(), [InvoicedItem("invoiced item", Decimal(20), Decimal(719))])
 
 renderer = PdfInvoiceRenderer("invoice")
-renderer.render(context)
+renderer.render(context, out_path)
--- src/invoice_utils/render/_render.py
+++ src/invoice_utils/render/_render.py
@@ -14,25 +14,18 @@ class PdfInvoiceRenderer:
         self.__jinja_template = template_name + ".jinja"
         self.__css_file = template_name + ".css"
 
-    def render(self, context: dict):
+    def render(self, context: dict, output_path: str):
         loader = jinja2.FileSystemLoader(searchpath=self.__templates_path)
         jinja_env = jinja2.Environment(loader=loader)
         jinja_env.filters["datetime_format"] = datetime_format
         html_template = jinja_env.get_template(self.__jinja_template)
 
-        items_by_currency = {}
-        for item in context["items"]:
-            item_no = item["item_no"]
-            currency_data = items_by_currency.get(item_no, [])
-            currency_data.append(item)
-            items_by_currency[item_no] = currency_data
-
         printer = weasyprint.HTML(string=html_template.render(
-            by_currency=items_by_currency, **context
+            **context
         ))
         css_file_path = self.__templates_path / self.__css_file
         stylesheet = weasyprint.CSS(filename=css_file_path)
         document = printer.render(stylesheets=[stylesheet])
-        with open(self.__templates_path.parent/"out.pdf", "wb") as f:
+        with open(output_path, "wb") as f:
             content = document.write_pdf()
             f.write(content)
