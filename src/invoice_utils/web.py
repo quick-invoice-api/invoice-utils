c2fbb6e9df9addc768d68d1975556f5b4e864d12
Adrian Moloce <adrian.moloce.dev@gmail.com>
2023-12-13 18:53:41 +0200

    feat: Create basic models for invoice request
    - implemented the body for the /invoice endpoint
    - InvoiceRequest, InvoiceEntity, InvoiceTaxInfo, InvoiceEntityBank,
      InvoiceRequestHeader

--- src/invoice_utils/web.py
+++ src/invoice_utils/web.py
@@ -1,8 +1,43 @@
+from datetime import datetime
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
+class InvoiceRequestHeader(BaseModel):
+    number: str
+    timestamp: datetime
+    items: list
+
+
+class InvoiceEntityBank(BaseModel):
+    iban: str
+    name: str
+
+
+class InvoiceTaxInfo(BaseModel):
+    id: str
+    registration_number: Optional[str] = None
+
+
+class InvoiceEntity(BaseModel):
+    name: str
+    address: str
+    bank: Optional[InvoiceEntityBank] = None
+    tax_info: InvoiceTaxInfo
+    admin_location: Optional[str] = None
+
+
+class InvoiceRequest(BaseModel):
+    header: InvoiceRequestHeader
+    buyer: InvoiceEntity
+    seller: InvoiceEntity
+
+
 @app.post("/invoice")
-def generate_invoice():
+def generate_invoice(request: InvoiceRequest):
     return {}
