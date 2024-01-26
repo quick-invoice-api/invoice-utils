f268df80c3e25672c3dddb69cb499e9e58da2806
Adrian Moloce <adrian.moloce.dev@gmail.com>
2024-01-26 19:05:19 +0200

    Feat/email notification (#4)
    * feat: Add httpx to dependency group.
    * feat: Add optional send_mail parameter.
    * feat: Log a message for requests with send_mail set to true
    * feat: Handle exception address not provided with send_mail true
      - Created handler for exception where address is not provided but
        send_mail is set to True
      - Updated and created new unit tests to test functionality
    * Send email notification with smtplib. (#5)
    * feat: Add pytest-mock and python-dotenv as dependencies
    * feat: Created config file with environment variables
    * feat: Send email notification with smtplib if send_mail
      - Send email with smtplib if send_mail flag is true
      - Created test for sending the mail and smtplib exceptions
      - Created custom InvoiceRequestEmailError
    * feat: Add note for setting up env variables in README
    * feat: Add namespace and default values to environment variables.
    * Feat: add basic configurations for sending emails. (#7)
    * feat: Config mail subject
    * feat: SMTP login
    - log into SMTP
    * feat: Make SMTP Login Optional
    * feat: Introduce SMTP start_tls optional env flag
    * fix: Increase resilience of smtp tls flag
      - method to convert string to bool for any env flag
      - added tests cases for the tls flag

--- README.md
+++ README.md
@@ -23,3 +23,13 @@ To run the app, you need to build the `runtime` image and then run that via
 `docker run`.
 To run the app's automated test suite you need to build the `test` image and
 run that using `docker run`.
+
+> [!NOTE]
+> **Note on allowing your Google account to send mails with SMTP:**
+> 
+> If you're not using app passwords already, go to [Google Account Security](https://myaccount.google.com/security?hl=en&nlr=1). If you have 2-step authentication enabled, then search for "App Passwords" and create a new password. You'll need to use this password for:
+> 
+> ```sh
+> INVOICE_UTILS_MAIL_LOGIN_PASSWORD = "app password"
+> INVOICE_UTILS_MAIL_LOGIN_USER = "e-mail address"
+> ```
--- src/invoice_utils/web.py
+++ src/invoice_utils/web.py
@@ -1,13 +1,24 @@
+import smtplib
 from datetime import datetime
+from dotenv import load_dotenv
+from logging import basicConfig, getLogger, DEBUG
 from pathlib import Path
+from sys import stdout
 from typing import Optional
+from email.mime.text import MIMEText
 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 from invoice_utils.engine import InvoicingEngine
+from invoice_utils.models import InvoicedItem
+import invoice_utils.config as config
 
 app = FastAPI()
+load_dotenv()
+basicConfig(stream=stdout, level=DEBUG)
+log = getLogger("invoice-utils")
 
 
 class InvoiceRequestHeader(BaseModel):
@@ -36,14 +47,70 @@ class InvoiceEntity(BaseModel):
 
 class InvoiceRequest(BaseModel):
     header: InvoiceRequestHeader
+    send_mail: bool = False
+    address: str = None
     buyer: InvoiceEntity
     seller: InvoiceEntity
-    items: list
+    items: list[InvoicedItem]
 
 
-@app.post("/invoice")
+class InvoiceRequestInputError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
+class InvoiceRequestEmailError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
+@app.exception_handler(InvoiceRequestInputError)
+def input_error_handler(request: InvoiceRequest, exc: InvoiceRequestInputError):
+    return JSONResponse(
+        status_code=422,
+        content={"message": f"{exc.message}"},
+    )
+
+
+@app.exception_handler(InvoiceRequestEmailError)
+def email_error_handler(request: InvoiceRequest, exc: InvoiceRequestEmailError):
+    return JSONResponse(status_code=502, content={"message": f"{exc.message}"})
+
+
+@app.post("/invoice", status_code=201)
 def generate_invoice(request: InvoiceRequest):
     root_dir = Path(__file__).parent
     basic_rules = str(root_dir / "basic.json")
     engine = InvoicingEngine(basic_rules)
-    return engine.process(int(request.header.number), request.header.timestamp, request.items)
+    _send_mail(request)
+    return engine.process(
+        int(request.header.number), request.header.timestamp, request.items
+    )
+
+
+def _send_mail(request):
+    if not request.send_mail:
+        return
+    if not request.address:
+        raise InvoiceRequestInputError(
+            "Address was not provided but send_mail is set to True."
+        )
+    message = _create_message(request)
+    try:
+        with smtplib.SMTP(config.INVOICE_UTILS_MAIL_HOST, config.INVOICE_UTILS_MAIL_PORT) as server:
+            if config.INVOICE_UTILS_SMTP_TLS:
+                server.starttls()
+            if config.INVOICE_UTILS_MAIL_LOGIN_USER is not None and config.INVOICE_UTILS_MAIL_LOGIN_PASSWORD is not None:
+                server.login(config.INVOICE_UTILS_MAIL_LOGIN_USER, config.INVOICE_UTILS_MAIL_LOGIN_PASSWORD)
+            server.sendmail(config.INVOICE_UTILS_SENDER_EMAIL, request.address, message.as_string())
+        log.info("Report was sent to %s", request.address)
+    except Exception as e:
+        raise InvoiceRequestEmailError("There was a problem sending the email.") from e
+
+
+def _create_message(request):
+    message = MIMEText("Empty Body", "html")
+    message["From"] = config.INVOICE_UTILS_SENDER_EMAIL
+    message["To"] = request.address
+    message["Subject"] = config.INVOICE_UTILS_MAIL_SUBJECT
+    return message
