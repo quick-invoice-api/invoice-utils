2ddfb019d2b8668af70d2dc1884993be3aff30aa
Andrei Olar <andrei.olar@gmail.com>
2025-08-30 11:05:47 +0300

    Fix displaying taxes (VAT) in the Romanian invoicing template (#25)
    * fix total tax display in the Romanian invoice template
    
    * add unit test for checking tax totals
    
    * bump patch version

--- tests/invoice_utils/engine/rule_engine_compute_taxes_test.py
+++ tests/invoice_utils/engine/rule_engine_compute_taxes_test.py
@@ -22,3 +22,17 @@ def test_process_tax_rule(basic_rules):
         "value": Decimal("2.300000")
     }]
     assert output["items"][0]["item_total"] == Decimal("12.000000")
+
+
+def test_process_computes_total_taxes(basic_rules):
+    engine = InvoicingEngine(basic_rules)
+
+    output = engine.process(
+        1, datetime.now(), [InvoicedItem("test", Decimal(2), Decimal(5))]
+    )
+
+    assert "taxes" in output["totals"]
+    assert output["totals"]["taxes"] == [{
+         "name": "vat",
+         "value": Decimal("2.0")
+    }]
